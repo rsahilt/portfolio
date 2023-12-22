6b48902a1dcd2e86d2b7cf769c117962c72a1520
iamsahilr <srajbhandari63@gmail.com>
2023-12-21 19:08:25 -0600

    added js animation package

--- iamsahilr/resources/views/frontend/index.blade.php
+++ iamsahilr/resources/views/frontend/index.blade.php
@@ -3,9 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- slide in animation js -->
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+
+    <!-- @vite('resources/css/app.css') -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
     <title>Sahil Rajbhandari</title>
 </head>
 <body>
-    
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+  	<script>
+    	AOS.init();
+  	</script>
 </body>
 </html>
