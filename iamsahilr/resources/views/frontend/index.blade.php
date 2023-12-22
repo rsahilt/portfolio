f36e1243319b956b5f1fac1fbe47756c71ed8d91
iamsahilr <srajbhandari63@gmail.com>
2023-12-21 21:00:49 -0600

    typing styled

--- iamsahilr/resources/views/frontend/index.blade.php
+++ iamsahilr/resources/views/frontend/index.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.main')
-@section('content')
-
-
+@section('contentmain')
+    <div class="typing-effect">
+        <h1>
+            <a href="" class="typewrite" data-period="2000" data-type='[ "Hi, I am Sahil Rajbhandari.", "I am a Developer."]'>
+                <span class="wrap"></span>
+            </a>
+        </h1>
+    </div>
 @endsection
--- iamsahilr/resources/views/layouts/main.blade.php
+++ iamsahilr/resources/views/layouts/main.blade.php
@@ -15,6 +15,10 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@300;400;600&family=Russo+One&display=swap" rel="stylesheet">
 
+    <!-- script for typing effect -->
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
     <!-- @vite('resources/css/app.css') -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
@@ -25,21 +29,21 @@
         </div> <!--main closed-->
 
         <div class="main-content">
-
+            @yield('contentmain')
         </div> <!--main-content closed-->
     </div>
     
     <div id="about" class="home-about-section" style="background-color:blue">
-        @yield('content');
+        @yield('contenthome')
     </div>
 
     <div id="resume" class="home-resume-section" style="background-color:red">
-        @yield('content');
+        @yield('contentresume')
     </div>
 
 
     <div id="work" class="home-work-section" style="background-color:green">
-        @yield('content');
+        @yield('contentabout')
     </div>
 
     <!-- script for animation -->
