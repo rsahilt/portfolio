cf9775110374a94aa723d7319b6bb555d1548176
iamsahilr <srajbhandari63@gmail.com>
2023-12-21 19:14:15 -0600

    installed bootstrap

--- iamsahilr/resources/views/frontend/index.blade.php
+++ iamsahilr/resources/views/frontend/index.blade.php
@@ -1,20 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- slide in animation js -->
-    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+@extends('layouts.main')
+@section('content')
 
-    <!-- @vite('resources/css/app.css') -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-    <title>Sahil Rajbhandari</title>
-</head>
-<body>
-    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
-  	<script>
-    	AOS.init();
-  	</script>
-</body>
-</html>
+@endsection
---
+++ iamsahilr/resources/views/layouts/main.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Sahil Rajbhandari</title>
+
+    <!-- slide in animation js -->
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+
+    <!-- @vite('resources/css/app.css') -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+  	<script>
+    	AOS.init();
+  	</script>
+</body>
+</html>
