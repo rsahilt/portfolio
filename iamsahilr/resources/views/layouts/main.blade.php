391c06ff03e9cabfa705dff4c2d7445a9c5bc6c3
iamsahilr <srajbhandari63@gmail.com>
2023-12-21 19:45:35 -0600

    created header

--- iamsahilr/resources/views/layouts/main.blade.php
+++ iamsahilr/resources/views/layouts/main.blade.php
@@ -10,14 +10,56 @@
     <!-- slide in animation js -->
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
 
+    <!-- google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@300;400;600&family=Russo+One&display=swap" rel="stylesheet">
+    
     <!-- @vite('resources/css/app.css') -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    @yield('content')
-    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
-  	<script>
-    	AOS.init();
-  	</script>
+    <div class="container-fluid">
+        <div class="main">
+            <div class="logo">
+                <h1>IAMSAHILR</h1>
+            </div> <!--logo closed-->
+
+            <nav class="navbar navbar-expand-lg">
+                
+                <!-- dropdown hamburger menu -->
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <!-- regular navbar -->
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">About</a>
+                        </li>
+
+                        <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Resume</a>
+                        </li>
+
+                        <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Works</a>
+                        </li>
+                        
+                    </ul>
+                </div>
+
+            </nav>
+
+        </div> <!--main closed-->
+        @yield('content')
+
+        <!-- script for animation -->
+        <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+        <script>
+            AOS.init();
+        </script>
+    </div>
 </body>
 </html>
