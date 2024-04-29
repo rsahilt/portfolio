0633537ca72ed5dfb135c23c19bd45863de25446
iamsahilr <srajbhandari63@gmail.com>
2024-04-29 09:33:55 -0500

    admin header created

--- iamsahilr/resources/views/admin/dashboard.blade.php
+++ iamsahilr/resources/views/admin/dashboard.blade.php
@@ -3,10 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- retrieving the $title parameter passed in index function in AdminController -->
     <title>{{ $title }}</title>
+
+    <!-- bootstrap connection link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- @vite('resources/css/app.css') -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom bg-custom-adm-header">
+        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+        <span class="fs-4">IAMSAHIR.</span>
+        </a>
+      
+        <!-- search box -->
+        <div class="form-outline" data-mdb-input-init>
+            <input type="search" id="form1" class="form-control" placeholder="Search"/>
+        </div>
+
+        <ul class="nav nav-pills">
+        <li class="nav-item"><a href="#" class="admin-nav nav-link" aria-current="page">Dashboard</a></li>
+        <li class="nav-item"><a href="#" class="admin-nav nav-link">Responses</a></li>
+        </ul>
+    </header>
 </body>
 </html>
