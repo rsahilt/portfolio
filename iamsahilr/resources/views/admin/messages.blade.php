5072ce42d7206e953cbad82f26769734ac1696b9
iamsahilr <srajbhandari63@gmail.com>
2024-05-09 11:45:19 -0500

    messages displayed from database

--- iamsahilr/resources/views/admin/messages.blade.php
+++ iamsahilr/resources/views/admin/messages.blade.php
@@ -5,14 +5,17 @@
 <div class="testimonials">
   
     <div class="container-fluid">
+        @foreach($messages as $message)
         <div class="review">
-            <div class="dp"></div>
+            <div class="dp">
+                <img class="dp-img-admin" src="/images/dp.png" alt="display picture">
+            </div>
             <div class="user">
-                <div class="username"></div>
-                <div class="userreview"></div>
+                <div class="username">{{ $message->username }}</div>
+                <div class="userreview">{{ $message->message }}</div>
             </div>
         </div>
-        
+        @endforeach
     </div>
 
 </div>
